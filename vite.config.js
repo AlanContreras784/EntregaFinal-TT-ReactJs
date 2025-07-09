3a398241694d7b166c3d1492e9a013395f059c19
AlanContreras784 <alancontreras784@gmail.com>
2025-07-09 00:46:50 -0300

    config vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: false, // desactiva los mapas de fuente en producción
+    minify: 'esbuild', // compresión más rápida y efectiva
+    cssCodeSplit: true, // divide los CSS por componentes
+    brotliSize: true,   // calcula tamaños para brotli (útil para medir)
+  },
+  server: {
+    port: 3000,
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
 })

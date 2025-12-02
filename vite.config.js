7b8350c992e2b1dc1145aaff58e7085a6e3c21e1
Alan Contreras <alancontreras74@gmail.com>
2025-12-02 00:55:35 -0300

    actualizaciones

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      fastRefresh: true
+    }),
+  ],
   build: {
     outDir: 'dist',
-    sourcemap: false,          // elimina mapas de fuente en producción
-    minify: 'esbuild',         // compresión rápida
-    cssCodeSplit: true,        // divide el CSS por componente
-    brotliSize: true,          // útil para medir compresión
-    chunkSizeWarningLimit: 1000, // sube el límite del warning de 500 KB a 1000 KB
+    sourcemap: false,
+    minify: 'esbuild',
+    cssCodeSplit: true,
+    brotliSize: true,
+    chunkSizeWarningLimit: 1000,
     rollupOptions: {
       output: {
         manualChunks(id) {
@@ -25,10 +28,14 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': '/src',
-    },
+      '@': '/src'
+    }
   },
   server: {
     port: 3000,
+    strictPort: true
   },
+  define: {
+    'process.env': {}
+  }
 })

e768490b1bd00786b0f8621d6af4216843a2c40e
Alan Contreras <alancontreras74@gmail.com>
2025-12-01 22:30:34 -0300

    cambios en la App.jsx y en el pack

--- src/App.jsx
+++ src/App.jsx
@@ -36,10 +36,10 @@ function App() {
 
   return (
     <Router
-      future={{
-        v7_startTransition: true,
-        v7_relativeSplatPath: true
-      }}
+      // future={{
+      //   v7_startTransition: true,
+      //   v7_relativeSplatPath: true
+      // }}
     >
       <Header />
 
